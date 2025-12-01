c368c2fdf1d14df60f0681949ed11e1e0e2b3bf3
shreya320 <119959515+shreya320@users.noreply.github.com>
2025-12-01 16:58:27 +0530

    Add files via upload

---
+++ bajaj/src/main/java/com/example/demo/BajajProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BajajProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BajajProjectApplication.class, args);
+	}
+
+}
